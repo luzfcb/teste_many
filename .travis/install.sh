9d2d45d7550d8d89b77ddc544927dc59d11ad860
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-06-03 22:23:35 -0300

    travis osx install

---
+++ .travis/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then fi
+
+    # Install some custom requirements on OS X
+    # e.g. brew install pyenv-virtualenv
+    brew install pyenv-virtualenv;
+    eval "$(pyenv init -)";
+    pyenv install python3.5
+    pyenv global python3.5
+    pip install -r requirements.txt
+    #case "${TOXENV}" in
+    #    py32)
+    #
+    #        ;;
+    #    py33)
+    #        # Install some custom Python 3.3 requirements on OS X
+    #        ;;
+    #esac
+else
+    # Install some custom requirements on Linux
+fi
