02f97c0b2ff3f6eeb2779b3ecb0526c5a7193d5d
Fabio C. Barrioneuvo da Luz <bnafta@gmail.com>
2016-06-03 22:26:13 -0300

    travis osx install

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then fi
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     # Install some custom requirements on OS X
     # e.g. brew install pyenv-virtualenv
